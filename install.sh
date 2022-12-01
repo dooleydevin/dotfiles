afa65225158a2f34b1dd8fc31b77f10455302468
Devin Dooley <dooleydevin@github.com>
2022-12-01 13:09:34 -0800

    Remove directories before cloning to prevent errors on re-install; fix shellcheck errors

--- install.sh
+++ install.sh
@@ -6,31 +6,35 @@ git config --global user.email "dooleydevin@github.com"
 git config --global core.editor vim
 
 # bash
-ln -sf $HOME/.dotfiles/bash/bashrc $HOME/.bashrc
-ln -sf $HOME/.dotfiles/bash/bash_profile $HOME/.bash_profile
+ln -sf "$HOME/.dotfiles/bash/bashrc" "$HOME/.bashrc"
+ln -sf "$HOME/.dotfiles/bash/bash_profile" "$HOME/.bash_profile"
 
 # zsh
 ## Install oh-my-zsh
-git clone https://github.com/ohmyzsh/ohmyzsh.git $HOME/.oh-my-zsh
+rm -rf "$HOME/.oh-my-zsh"
+git clone https://github.com/ohmyzsh/ohmyzsh.git "$HOME/.oh-my-zsh"
 export ZSH="${HOME}/.oh-my-zsh"
 export ZSH_CUSTOM="${HOME}/.oh-my-zsh/custom"
 
 ## Install powerlevel10k plugin
-git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
+rm -rf "$ZSH_CUSTOM/themes/powerlevel10k"
+git clone --depth=1 https://github.com/romkatv/powerlevel10k.git "$ZSH_CUSTOM/.oh-my-zsh/custom/themes/powerlevel10k"
 
 ## Install zsh-vi-mode plugin
-git clone https://github.com/jeffreytse/zsh-vi-mode $ZSH_CUSTOM/plugins/zsh-vi-mode
+rm -rf "$ZSH_CUSTOM/plugins/zsh-vi-mode"
+git clone https://github.com/jeffreytse/zsh-vi-mode "$ZSH_CUSTOM/plugins/zsh-vi-mode"
 
 ## Link zsh config
-ln -sf $HOME/.dotfiles/zsh/zshrc $HOME/.zshrc
-ln -sf $HOME/.dotfiles/zsh/p10k.zsh $HOME/.p10k.zsh
+ln -sf "$HOME/.dotfiles/zsh/zshrc" "$HOME/.zshrc"
+ln -sf "$HOME/.dotfiles/zsh/p10k.zsh" "$HOME/.p10k.zsh"
 
 # vim
 ## Install vim-plug
-sh -c 'curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
+rm -rf ~/.vim/autoload/plug.vim
+curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 
 ## Link vimrc
-ln -sf $HOME/.dotfiles/vim/vimrc $HOME/.vimrc
+ln -sf "$HOME/.dotfiles/vim/vimrc" "$HOME/.vimrc"
 
 ## Install plugins with vim-plug
 vim +'PlugInstall --sync' +qall
--- uninstall.sh
+++ uninstall.sh
@@ -6,13 +6,13 @@
 #  to accurately test a configuration installation.
 
 # Delete bash configuration
-rm $HOME/.bashrc
-rm $HOME/.bash_profile
+rm "$HOME/.bashrc"
+rm "$HOME/.bash_profile"
 
 # Delete zsh configuration
-rm -rf $HOME/.oh-my-zsh
-rm $HOME/.zshrc
-rm $HOME/.p10k.zsh
+rm -rf "$HOME/.oh-my-zsh"
+rm "$HOME/.zshrc"
+rm "$HOME/.p10k.zsh"
 
 # Delete vim configuration
-rm $HOME/.vimrc
+rm "$HOME/.vimrc"

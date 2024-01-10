d66fc782d0bf193598ebb8b632da36d779e88ee8
Devin Dooley <dooleydevin@github.com>
2024-01-09 21:22:38 -0800

    Install silently

--- install.sh
+++ install.sh
@@ -16,17 +16,17 @@ ln -sf "$DIR_NAME/bash/bash_profile" "$HOME/.bash_profile"
 # zsh
 ## Install oh-my-zsh
 rm -rf "$HOME/.oh-my-zsh"
-git clone https://github.com/ohmyzsh/ohmyzsh.git "$HOME/.oh-my-zsh"
+git clone https://github.com/ohmyzsh/ohmyzsh.git "$HOME/.oh-my-zsh" --quiet
 export ZSH="${HOME}/.oh-my-zsh"
 export ZSH_CUSTOM="${HOME}/.oh-my-zsh/custom"
 
 ## Install powerlevel10k plugin
 rm -rf "$ZSH_CUSTOM/themes/powerlevel10k"
-git clone --depth=1 https://github.com/romkatv/powerlevel10k.git "$ZSH_CUSTOM/themes/powerlevel10k"
+git clone --depth=1 https://github.com/romkatv/powerlevel10k.git "$ZSH_CUSTOM/themes/powerlevel10k" --quiet
 
 ## Install zsh-vi-mode plugin
 rm -rf "$ZSH_CUSTOM/plugins/zsh-vi-mode"
-git clone https://github.com/jeffreytse/zsh-vi-mode "$ZSH_CUSTOM/plugins/zsh-vi-mode"
+git clone https://github.com/jeffreytse/zsh-vi-mode "$ZSH_CUSTOM/plugins/zsh-vi-mode" --quiet
 
 ## Link zsh config
 ln -sf "$DIR_NAME/zsh/zshrc" "$HOME/.zshrc"
@@ -35,7 +35,7 @@ ln -sf "$DIR_NAME/zsh/p10k.zsh" "$HOME/.p10k.zsh"
 # vim
 ## Install vim-plug
 rm -rf ~/.vim/autoload/plug.vim
-curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+curl -fsLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 
 ## Link vimrc
 ln -sf "$DIR_NAME/vim/vimrc" "$HOME/.vimrc"
